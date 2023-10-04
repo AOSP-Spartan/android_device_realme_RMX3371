768c1a768c94aaa6037ef0dbd48096f66ea79390
Blackmanx <blackman@sajl.tk>
2023-10-04 20:10:39 +0200

    RMX3371: Import and PATCHELF libgui1_vendor.so
    If patchelf isn't installed locally this doesn't work, so libui isn't patched and audio ends up dead.

--- extract-files.py
+++ extract-files.py
@@ -119,6 +119,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
         .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
+    'vendor/lib/libgui1_vendor.so': blob_fixup()
+        .replace_needed('libui.so', 'libui-v30.so'),
     'vendor/lib64/hw/camera.qcom.so': blob_fixup()
         .add_needed('libcamera_metadata_shim.so'),
     'odm/lib64/libOGLManager.so': blob_fixup()
