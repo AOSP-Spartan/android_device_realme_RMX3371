ab5236c7e4f87c91f5da3c037252e3add83001d5
ryukftw <ryukv009@gmail.com>
2025-05-22 08:41:35 +0000

    RMX3371: Disable Play Integrity Hooks

--- device.mk
+++ device.mk
@@ -288,6 +288,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# PI Hooks
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.pihooks.disable.gms_props=true
+    persist.sys.pihooks.disable.gms_key_attestation_block=true
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
