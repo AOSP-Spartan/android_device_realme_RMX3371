5d304804af982f9705b3d9430e862b0acb82ddd5
ryukftw <ryukv009@gmail.com>
2025-06-20 03:59:36 +0000

    Revert "sm8250-common: Set UDFPS extension lib via soong config"
    This reverts commit 8fc6966e9258f7549ce9c72d71768a2ccaa11359.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,6 +69,9 @@ TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
+# Fingerprint
+TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/oplus/vintf/device_framework_matrix.xml \
