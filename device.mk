023a247ada58b69d929b8dc050d4b52a31537c1f
ryukftw <ryukv009@gmail.com>
2025-06-20 17:28:55 +0000

    Revert "sm8250-common: Migrate Lineage Health to soong_config_set"
    This reverts commit 2445669e4df3c88e1b9458f2cc1c7a6fc79ab406.

--- device.mk
+++ device.mk
@@ -222,8 +222,6 @@ $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_oplus)
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/mmi_charging_enable)
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oplus
