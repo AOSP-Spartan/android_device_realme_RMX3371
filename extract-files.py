XXBADRGXX/device_realme_bitra

RMX3371: Patch libAlgoProcess to resolve duplicate dependencies
Fixes:
error: module "libAlgoProcess" variant
"android_vendor_arm64_armv9-a_static": depends on multiple
versions of the same aidl_interface:
android.hardware.graphics.common-V3-ndk-source,
android.hardware.graphics.common-V5-ndk-source

Change-Id: If0be3da5c13e45c6c59dde98bb80f35eb22bf846

extract-files.py
@@ -131,6 +131,8 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(b'com.oem.autotest', b'\x00om.oem.autotest'),
     'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
         .call(blob_fixup_nop_call, 'bl', '__cfi_check', '_ZN7android8hardware22configureRpcThreadpoolEmb@plt'),
+    'odm/lib64/libAlgoProcess.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V5-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(

AOSP-Spartan/android_device_realme_RMX3371

extract-files.py
@@ -120,6 +120,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
         .add_needed('libcamera_metadata_shim.so')
         .binary_regex_replace(b'com.oem.autotest', b'\x00om.oem.autotest'),
+    'odm/lib64/libAlgoProcess.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V6-ndk.so'),
     'odm/lib64/libOGLManager.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')